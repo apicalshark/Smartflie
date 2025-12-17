ba248d13e460dce9f95694943f504444ebc14046
apicalshark <dennyxbox890@gmail.com>
2025-12-17 14:39:02 +0800

    final?

--- src/main.cpp
+++ src/main.cpp
@@ -9,3 +9,10 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    QApplication main(__argc, __argv);
+    MainWindow window;
+    window.show();
+    return main(__argc, __argv);
+}
