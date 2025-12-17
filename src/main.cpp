6099203f4dbe13351ff9c313f48bbe53130c56bf
apicalshark <dennyxbox890@gmail.com>
2025-12-17 14:56:48 +0800

    b

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,14 @@ int main(int argc, char *argv[])
     return a.exec();
 }
 
-// Windows entry point, checks win32
-#ifdef _WIN32
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    int argc = __argc;
-    char **argv = __argv;
-    QApplication main(argc, argv);
-    MainWindow window;
-    window.show();
-    return main.exec();
-}
-#endif // _WIN32
+// // Windows entry point, checks win32
+// #ifdef _WIN32
+// int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+//     int argc = __argc;
+//     char **argv = __argv;
+//     QApplication main(argc, argv);
+//     MainWindow window;
+//     window.show();
+//     return main.exec();
+// }
+// #endif // _WIN32
